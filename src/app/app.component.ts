346f1943c92ba7f224440941f38b9d7941c1634e
Paulo Cesar <paulo.csa@outlook.com>
2022-12-07 12:44:42 -0300

    atulização de funçoes menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ɵisListLikeIterable } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'APVS';
+  
+  abrirMenu(){
+      
+    let navBar = document.querySelector(".menu");
+    navBar?.classList.toggle("mostrarMenu");
+          
+  }
+
+  fecharMenu(){
+      
+    let navBar = document.querySelector(".mostrarMenu");
+    navBar?.classList.remove("fecharMenu");
+    navBar?.classList.toggle("mostrarMenu");
+          
+  }
 }
+
